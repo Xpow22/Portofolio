3bb45a11868905996bfcc6e3c17b784839a8a658
Diska <diskaesfandiary22@gmail.com>
2023-12-25 15:35:11 +0700

    second commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,23 +5,23 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "Immersive",
+    title: "Immersive Apps",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "Immersive Apps is an application for mentee management, including mentee lists, mentee classes, user lists, and a page for feedback from mentors to mentees. The technology used is ReactJS, and we also utilize RESTful API and FIGMA for UI/UX design.",
     demoLink: "https://github.com/Xpow22/Immersive-App",
   },
   {
     id: 2,
     title: "HRIS Apps",
     img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "HRIS-App is an application used for employee management in terms of submitting time-off, reimbursement, attendance, and HR management. The technology used is ReactJS and Redux, and we also use RESTful API and FIGMA for UI/UX design.",
     demoLink: "https://github.com/HRIS-APP-TEAM-3/Frontend_immersive",
   },
   {
     id: 3,
     title: "EVVE Apps",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "EVVE is an online ticket sales application that makes it easy for users to buy tickets without the need to come to the location and maintain social distance because of the use of QR codes as a substitute for traditional tickets. This project uses technology from React.js and RESTful API from the Backend team.",
     demoLink: "https://github.com/Capstone-Project-Team-2/Frontend-Immersive",
   },
   
